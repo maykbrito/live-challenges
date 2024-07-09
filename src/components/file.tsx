5fcd618436d4225bea4c1a94d9edf57ef500a3ea
Mayk Brito <maykbrito@gmail.com>
2024-07-09 20:01:25 -0300

    adjusting footer

--- src/components/file.tsx
+++ src/components/file.tsx
@@ -12,7 +12,7 @@ export function File(props: FileProps) {
   return (
     <div className="ml-4 mt-1">
       <a href={props.href}>
-        <div className={cn("flex items-center gap-2 px-2 py-1 w-full border rounded-sm   border-transparent text-slate-200 text-xs hover:bg-gray-800 hover:text-[#E0DEF2] focus:bg-gray-800 focus:border-[#363247] focus:text-[#e0def2] outline-none", { 'bg-gray-800 text-white': isCurrentActive })}>
+        <div className={cn("flex items-center gap-2 px-2 py-1 w-full border rounded-sm   border-transparent text-slate-200 text-sm hover:bg-gray-800 hover:text-[#E0DEF2] focus:bg-gray-800 focus:border-[#363247] focus:text-[#e0def2] outline-none", { 'bg-gray-800 text-white': isCurrentActive })}>
           <FileIcon className="mr-1 h-4 w-4" />
           <span>{props.children}</span>
         </div>
--- src/components/folder.tsx
+++ src/components/folder.tsx
@@ -12,7 +12,7 @@ export function Folder({title, children, defaultOpen = false}: FolderProps) {
   return (
     <Collapsible.Root open={isOpen} onOpenChange={setIsOpen}>
     
-      <Collapsible.Trigger className="flex items-center gap-2 px-2 py-1 w-full border rounded-sm   border-transparent text-slate-200 text-xs hover:bg-gray-800 hover:text-[#E0DEF2] focus:bg-gray-800 focus:border-[#363247] focus:text-[#e0def2] outline-none">
+      <Collapsible.Trigger className="flex items-center gap-2 px-2 py-1 w-full border rounded-sm   border-transparent text-slate-200 text-sm hover:bg-gray-800 hover:text-[#E0DEF2] focus:bg-gray-800 focus:border-[#363247] focus:text-[#e0def2] outline-none">
         {isOpen ? <ChevronDownIcon className="mr-1 h-4 w-4" /> : <ChevronRightIcon className="mr-1 h-4 w-4" /> }
 
         <span>{title}</span>
